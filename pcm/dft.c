b15faa619bf94e971c1f737889ff623e902abbcc
K47de <git@k4na.de>
2026-05-18 09:51:05 +0900

    Validate DFT window inputs and fix df precision

--- pcm/dft.c
+++ pcm/dft.c
@@ -75,6 +75,27 @@ int main(int argc, char *argv[])
 	debug("■ 入力データ\n");
 	pcmInfo(stderr, p);
 
+	if (N <= 0) {
+		debug("エラー：標本数Nは正の整数が必要です（N=%d）\n", N);
+		pcmFin(p);
+		return (1);
+	}
+	if (n0 < 0) {
+		debug("エラー：始点n0は0以上が必要です（n0=%d）\n", n0);
+		pcmFin(p);
+		return (1);
+	}
+	if (p->fmt.fs == 0) {
+		debug("エラー：標本化周波数fsが不正です（fs=%u）\n", p->fmt.fs);
+		pcmFin(p);
+		return (1);
+	}
+	if ((unsigned long long)n0 + (unsigned long long)N > (unsigned long long)p->len) {
+		debug("エラー：分析区間がデータ長を超えています（n0=%d, N=%d, len=%u）\n", n0, N, p->len);
+		pcmFin(p);
+		return (1);
+	}
+
 	debug("■ 分析条件\n");
 	debug("開始番号 = %d，\t開始時刻 = %f [s]\n", n0, (double)n0/p->fmt.fs);
 	debug("標本数 = %d，\t窓幅 = %f [s]\n", N, (double)N/p->fmt.fs);
@@ -83,7 +104,7 @@ int main(int argc, char *argv[])
 
 	Cx	x[N], X[N];		// 波形とスペクトルのバッファ
 	double	dt = 1.0/p->fmt.fs;	// Δt = 1/f_s
-	double	df = p->fmt.fs/N;	// Δf = f_s//N
+	double	df = (double)p->fmt.fs/(double)N;	// Δf = f_s/N
 	double	*v = &(p->val[0][n0]);		// 分析対象の標本値列の先頭アドレス
 	for (int n = 0; n < N; n++) x[n] = v[n];	// 分析対象をバッファへコピー
 	pcmFin(p);
